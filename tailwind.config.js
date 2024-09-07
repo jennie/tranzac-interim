0fb8521f34187d0c9c767acf3ca9bcdbab411de4
Jennie Robinson Faber <jennie@jenniefaber.com>
2024-09-07 14:48:55 -0400

    Styles and content

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "orange-soda": "#F35A3F",
+        cherokee: "#F5CE85",
+        marigold: "#E99D21",
+        "eerie-black": "#1C1C20",
+        "black-haze": "#DFDFD6",
+      },
+      fontFamily: {
+        "dm-sans": ["DM Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
